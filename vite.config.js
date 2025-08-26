30d7161c554ac043704f4c5785d0862a44d417cd
Mostafa <mosstafagenedy@gmail.com>
2025-08-26 22:53:35 +0300

    magic cursor css name bug fix

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js','resources/css/magiccursor.css', 'resources/js/magiccursor.js'],
+            input: ['resources/css/app.css', 'resources/js/app.js','resources/css/mousecursor.css', 'resources/js/magiccursor.js'],
             refresh: true,
         }),
     ],
