4de8479cc7581532ec347ffcf9b7853fdbe225e0
Mostafa <mosstafagenedy@gmail.com>
2025-11-21 00:21:58 +0200

    added canonical link for seo

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,6 +6,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>{{ config('app.name', 'Lalune By NE') }}</title>
+    <link rel="canonical" href="{{ url()->current() }}" />
+
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
